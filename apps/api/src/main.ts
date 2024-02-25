317005885a1daa88af9021cdab993fd384207232
Kamil <kprzybylski993@gmail.com>
2024-02-25 19:17:38 +0100

    init app

---
+++ apps/api/src/main.ts
@@ -0,0 +1,28 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // const cs = app.get(ConfigService);
+  console.log(666.1, process.env.DATABASE_USER)
+  console.log(666.2, process.env.DATABASE_PASSWORD)
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
