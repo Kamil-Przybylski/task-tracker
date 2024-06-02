1eee49db9aa8b60c382174a0eaa0cc582d92f359
Kamil <kprzybylski993@gmail.com>
2024-06-02 23:35:25 +0200

    prettier fixes

--- libs/shared/web/src/lib/local-storage/local-storage.ts
+++ libs/shared/web/src/lib/local-storage/local-storage.ts
@@ -1,11 +1,16 @@
 import { ILocalStorage } from './local-storage.models';
 
 export abstract class LocalStorage {
-  static getItem<T extends keyof ILocalStorage>(key: T): ILocalStorage[T] | null {
+  static getItem<T extends keyof ILocalStorage>(
+    key: T,
+  ): ILocalStorage[T] | null {
     return JSON.parse(localStorage.getItem(key) as ILocalStorage[T]);
   }
 
-  static setItem<T extends keyof ILocalStorage>(key: T, data: ILocalStorage[T]): void {
+  static setItem<T extends keyof ILocalStorage>(
+    key: T,
+    data: ILocalStorage[T],
+  ): void {
     localStorage.setItem(key, JSON.stringify(data));
   }
 
